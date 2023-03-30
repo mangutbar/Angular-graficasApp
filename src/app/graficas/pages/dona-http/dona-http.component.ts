a3561e1dc7d7cd10d002363cbbb655ec66c6fa2d
Manuel Gutierrez <magutbar@gmail.com>
2023-03-30 09:39:28 +0200

    graficasApp terminada

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -9,14 +10,56 @@ import { GraficasService } from '../../services/graficas.service';
 })
 export class DonaHttpComponent implements OnInit {
 
+  public doughnutChartLabels: string[] = [ 
+    // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Other' 
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 
+        // 350, 450, 100, 150 
+      ],
+        backgroundColor: ['#72E87A','#F56143', '#70BDFF', '#E0A555'],
+        hoverBackgroundColor: ['#72E87A','#F56143', '#70BDFF', '#E0A555'],
+        hoverBorderColor: ['#72E87A','#F56143', '#70BDFF', '#E0A555']
+      }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+
+
   constructor(private graficasService: GraficasService) { }
 
   ngOnInit(): void {
 
-    this.graficasService.getUsuariosRedesSociales()
-      .subscribe( data => {
-        console.log(data);
-      })
+    // this.graficasService.getUsuariosRedesSociales()
+    //   .subscribe( data => {
+    //     const labels = Object.keys( data );
+    //     const values = Object.values( data );
+    //     this.doughnutChartLabels = labels;
+    //     this.doughnutChartData = { 
+    //       labels: this.doughnutChartLabels,
+    //       datasets: [
+    //         {
+    //           data: values
+    //         }
+    //       ]
+    //     }
+    //   });
+
+    this.graficasService.getUsuariosRedesSocialesDonaData()
+        .subscribe( ({ labels, values }) => {
+          this.doughnutChartLabels = labels;
+          this.doughnutChartData = { 
+             labels: this.doughnutChartLabels,
+             datasets: [
+               {
+                 data: values
+               }
+             ]
+           }
+        });
   }
 
 }
